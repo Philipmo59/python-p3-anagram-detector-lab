f471a46f7752f153e122b79cf9904a27cde0db89
philipmo <philipmo59@gmail.com>
2023-09-24 12:03:41 -0400

    finished

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,21 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word) -> None:
+        self.word = word
+    def match(self, list):
+        self.list = list
+        matched_list = []
+        for word in self.list:
+            if len(word) != len(self.word):
+                pass
+            else:
+                    if sorted(word.lower()) == sorted(self.word.lower()):
+                        print(word)
+                        matched_list.append(word)
+                    else:
+                         return matched_list
+        print(matched_list)
+        return matched_list
+    
+
+apple = Anagram("Apple")
+apple.match(['pplea',"efefefw","apwife","leapp"])
